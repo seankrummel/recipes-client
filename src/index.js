a1809d5f2457645fc9cabd68a6623ba4644eddb2
seankrummel <sean.krummel@gmail.com>
2019-01-09 15:05:39 -0600

    added basic css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import App from './App';
 import store from './store'
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
